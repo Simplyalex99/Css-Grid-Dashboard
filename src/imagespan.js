b0195bb7c8f6700b8d3c463c815483359353ee06
Simplyalex99 <alexmflores99@hotmail.ca>
2020-08-21 17:34:31 -0400

    Move fetch to seperate file and minor changes
    Changed source link attrbiute location to bottom of html script to imrpove time and speed of page loading.
    
    Also minor changes to structure of code

--- src/imagespan.js
+++ src/imagespan.js
@@ -1,62 +1,23 @@
 var routesDictionary = new Map();
 
-document.addEventListener("DOMContentLoaded",()=>{
-const divs = document.getElementsByTagName("div");
-const numberOfDivs = divs.length;
+ document.addEventListener("DOMContentLoaded", () => {
+  const divs = document.getElementsByTagName("div");
+  const numberOfDivs = divs.length;
 
-/*Add text file for pasting all urls and retrieve them and add them as the url 
-As for size get an array of divs inside containers and thats the size
-*/
+  for (let i = 1; i < numberOfDivs; i++) {
+    let url = "";
+    routesDictionary.set(divs[i].id, url);
+  }
 
-for (let i = 1; i < numberOfDivs; i++) {
-  let url = "";
-  routesDictionary.set(divs[i].id, url);
-}
-
-
-})
-
-
-/*Add text file for pasting all urls and retrieve them and add them as the url 
-As for size get an array of divs inside containers and thats the size
-*/
-
-
+});
 
 function directUserToURL(id_of_div) {
   let url = routesDictionary.get(id_of_div);
   window.open(url);
 }
- 
 
-async function setWeather() {
-  const KELVIN = 273;
-  const API_KEY = "";
-  let coordinates = getLocation();
-  let lat = coordinates.lat;
-  let lon = coordinates.lon;
-  let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  let response = await fetch(url);
-  let body = await response.json();
-  let temperature = Math.floor(body.main.temp - KELVIN);
-  document
-    .getElementById("box-6")
-    .getElementsByTagName("h2")[0].innerHtml = `${temperature}  + &deg`;
-}
 
- function getLocation () {
-  let location = {
-    lat: 0,
-    lon: 0,
-  };
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      location = {
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
-      };
-    });
 
-    return location;
-  }
-}
+
+
+
---
+++ src/weather.js
@@ -0,0 +1,35 @@
+
+
+var lon = 0;
+var lat = 0;
+
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+
+  const KELVIN = 273;
+ const API_KEY = "";
+
+  let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  
+  try {
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+        }
+        return res.json();
+      })
+      .then((body) => {
+        console.log(body);
+        let temperature = Math.floor(body.list[0].main.temp - KELVIN);
+        document
+          .getElementById("box4")
+          .getElementsByTagName("h2")[0].innerText = `${temperature}\xB0`; // not setting
+      });
+  } catch (err) {}
+}
+function error(err) {}
